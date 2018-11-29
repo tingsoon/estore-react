a305d10dfa7fd52b0cbc5abb487c790c52f08459
Ting Soon <angts8@hotmail.com>
2018-11-29 12:21:24 +0800

    added some stuff

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,19 +1,66 @@
-import React from "react";
+import React, { Component } from "react";
 import ProductListItem from "./ProductListItem";
 
-export default props => {
-  const renderProducts = props.products.map(product => (
-    <ProductListItem
-      key={product.id}
-      product={product}
-      clickHandler={props.sellItem}
-    />
-  ));
+class ProductList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: [
+        {
+          id: 1,
+          title: "iphone xs",
+          price: 1800,
+          stock: 20
+        },
+        {
+          id: 2,
+          title: "iphone xs max",
+          price: 2000,
+          stock: 20
+        },
+        {
+          id: 3,
+          title: "Pixel 3",
+          price: 1200,
+          stock: 20
+        }
+      ]
+    };
+    this.handleClick = this.handleClick.bind(this);
+    this.onSale = this.onSale.bind(this);
+  }
 
-  return (
-    <div>
-      <h3>Products</h3>
-      {renderProducts}
-    </div>
-  );
-};
+  onSale(id) {
+    let index = this.state.products.findIndex(p => p.id === id);
+    let newProduct = { ...this.state.products[index] };
+    newProduct.stock--;
+    this.state.products[index] = newProduct;
+    this.setState({ products: this.state.products });
+  }
+
+  _renderProducts() {
+    return this.state.products.map(p => (
+      <ProductListItem key={p.id} product={p} onSale={this.onSale} />
+    ));
+  }
+
+  handleClick(e) {
+    let products = [
+      ...this.state.products,
+      { id: 4, title: "nexus", price: 800, stock: 30 }
+    ];
+    this.setState({ products });
+  }
+  render() {
+    console.log("render");
+    return (
+      <div>
+        <button onClick={this.handleClick}>Add Product</button>
+        <h1>Products</h1>
+        {this._renderProducts()}
+      </div>
+    );
+  }
+}
+
+export default ProductList;
--- src/components/ProductListItem.js
+++ src/components/ProductListItem.js
@@ -1,14 +1,35 @@
-import React from "react";
+import React, { Component, PureComponent } from "react";
 
-export default props => {
-  const { id, title, price, stock } = props.product;
-  return (
-    <div>
-      <h4>{title}</h4>
-      <p>Price: ${price}</p>
-      <p>Stocks: {stock}pcs</p>
-      <button onClick={() => props.clickHandler(id)}>Buy this</button>
-      <hr />
-    </div>
-  );
-};
+class ProductListItem extends PureComponent {
+  constructor() {
+    super();
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  // shouldComponentUpdate(nextProps, nextState) {
+  //   // console.log("nextProps", nextProps);
+  //   // console.log("this.props", this.props);``
+  //   return this.props.product != nextProps.product;
+  // }
+
+  handleClick() {
+    this.props.onSale(this.props.product.id);
+  }
+
+  render() {
+    let { title, stock, price } = this.props.product;
+    return (
+      <div>
+        <h2>
+          item: {title}<br/>
+          stock: ({stock})<br/>
+          time: {new Date().toISOString()}<br/>
+        </h2>
+        <h2>{price}</h2>
+        <button onClick={this.handleClick}>SELL</button>
+      </div>
+    );
+  }
+}
+
+export default ProductListItem;
